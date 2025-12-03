bcf9257dcb6a7423ee0bdf4527fb8c5e4668a2fe
NeroClaudia <itzfaraways10@gmail.com>
2025-12-03 22:49:04 +0700

    display audience name on the 3rd line using foreach loop

--- Cinema07.java
+++ Cinema07.java
@@ -18,8 +18,8 @@ public class Cinema07 {
             System.out.println("Lenght of row " + (i + 1) + ": " + audience[i].length);
         }
         System.out.println("Audiences in the row 3: ");
-        for (int i = 0; i < audience[2].length; i++) {
-            System.out.println(audience[2][i]);
+        for (String name : audience[2]) {
+            System.out.println(name);
         }
     }
 }
