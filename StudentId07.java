a64f89bd13801c98b599d539fd2537f87563881c
NeroClaudia <itzfaraways10@gmail.com>
2025-12-04 09:16:57 +0700

    Ver 2

--- StudentId07.java
+++ StudentId07.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StudentId07 {
     public static void main(String[] args) {
         
@@ -5,6 +7,13 @@ public class StudentId07 {
         myNumbers[0] = new int [5];
         myNumbers[1] = new int [3]; 
         myNumbers[2] = new int [1]; 
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Length of row " + (i + 1) + ": " + myNumbers[i].length);
+        }
 
     }
 }
